7be9e3ea6e0213d3f91c78bf46cd1d7d5383722e
B-S200502 <basemsousou02@gmail.com>
2024-07-04 18:41:59 -0400

    Basem-Ayman Merge
    I manually merged all of my files that i created for the home page (and the ones that help us redirect to other pages). I synced up everything so that the application is fully functional. Just need to figure out the theme bug. But everything is working

---
+++ app/src/main/java/com/example/nsgs_app/LocationActivity.java
@@ -0,0 +1,33 @@
+package com.example.nsgs_app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LocationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location);
+
+        // Enable the Up button
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            // Handle the back button press
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
