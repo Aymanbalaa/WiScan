0aef7f02fb19a22fa8120a507bd41916e1650b5a
Aymanbalaa <84538300+Aymanbalaa@users.noreply.github.com>
2024-07-05 15:25:37 -0400

    remove bottom tool bar and hello user, added settings button

--- app/src/main/java/com/example/nsgs_app/MainActivity.java
+++ app/src/main/java/com/example/nsgs_app/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
         Button guidanceButton = findViewById(R.id.guidance_button);
         Button wifiButton = findViewById(R.id.wifi_button);
         Button locationButton = findViewById(R.id.location_button);
+        Button settingsButton = findViewById(R.id.settings_button);
 
         guidanceButton.setOnClickListener(v -> {
             Toast.makeText(MainActivity.this, "Not available yet", Toast.LENGTH_SHORT).show();
@@ -29,6 +30,12 @@ public class MainActivity extends AppCompatActivity {
         });
 
         locationButton.setOnClickListener(v -> {
+            Toast.makeText(MainActivity.this, "Not available yet", Toast.LENGTH_SHORT).show();
+//            Intent intent = new Intent(MainActivity.this, LocationActivity.class);
+//            startActivity(intent);
+        });
+
+        settingsButton.setOnClickListener(v -> {
             Toast.makeText(MainActivity.this, "Not available yet", Toast.LENGTH_SHORT).show();
 //            Intent intent = new Intent(MainActivity.this, LocationActivity.class);
 //            startActivity(intent);
