d2a3b02f14a110906cac7111d68170a39b2d42e5
Captaintom14 <60295341+Captaintom14@users.noreply.github.com>
2024-07-15 00:46:31 -0400

    Update the Language Class
    setting up the Language class with the use of the sharedPreference functionality

--- app/src/main/java/com/example/nsgs_app/Language.java
+++ app/src/main/java/com/example/nsgs_app/Language.java
@@ -1,4 +1,35 @@
 package com.example.nsgs_app;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+
+import java.util.Locale;
+
+
 public class Language {
+
+    private SharedPreferences sharedPreferences;
+
+    public void saveLanguage(Context context, String langCode){
+        sharedPreferences = context.getSharedPreferences("prefs", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("language", langCode);
+        editor.apply();
+    }
+
+    public static void setLanguage(Context context, String langCode){
+
+        Locale locale = new Locale(langCode);
+        Locale.setDefault(locale);
+        Resources resources = context.getResources();
+        Configuration conf = resources.getConfiguration();
+        conf.setLocale(locale);
+        resources.updateConfiguration(conf,resources.getDisplayMetrics());
+
+    }
+
+
+
 }
