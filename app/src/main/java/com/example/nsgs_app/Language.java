70d44ec5fc9f087cb2ac19c576421613c21e1403
Captaintom14 <60295341+Captaintom14@users.noreply.github.com>
2024-07-16 00:22:38 -0400

    Progress on the TemperaturePreference
    -We're gonna use the sharedPreference so that it can do the job as usual.

--- app/src/main/java/com/example/nsgs_app/Language.java
+++ app/src/main/java/com/example/nsgs_app/Language.java
@@ -27,4 +27,7 @@ public class Language {
         resources.updateConfiguration(conf,resources.getDisplayMetrics());
     }
 
+
+
+
 }
--- app/src/main/java/com/example/nsgs_app/TemperaturePreference.java
+++ app/src/main/java/com/example/nsgs_app/TemperaturePreference.java
@@ -1,10 +1,18 @@
 package com.example.nsgs_app;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 public class TemperaturePreference {
 
+    public static void saveTemperatureUnit (Context context,String temperatureUnit){
 
-
-
-
-
+        SharedPreferences sharedPreferences = context.getSharedPreferences("temperature_unit", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("temperature", temperatureUnit);
+        editor.apply();
+    }
 }
+
+
+
