51ac984d96233a80f2c7bb71951afdd5cf2ed46d
B-S200502 <basemsousou02@gmail.com>
2024-08-01 11:18:00 +0300

    Added information to Additional Info

--- app/src/main/java/com/example/nsgs_app/AdditionalInfoActivity.java
+++ app/src/main/java/com/example/nsgs_app/AdditionalInfoActivity.java
@@ -1,4 +1,27 @@
 package com.example.nsgs_app;
 
-public class AdditionalInfoActivity {
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdditionalInfoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_additional_info);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish(); // Go back to the previous activity
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
