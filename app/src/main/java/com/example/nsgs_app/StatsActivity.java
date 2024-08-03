7eeb2fc990f5f8fb9bf56cc118de2e259dffdf34
Aymanbalaa <84538300+Aymanbalaa@users.noreply.github.com>
2024-08-03 02:13:23 -0400

    fixing bugs in theme

--- app/src/main/java/com/example/nsgs_app/StatsActivity.java
+++ app/src/main/java/com/example/nsgs_app/StatsActivity.java
@@ -15,10 +15,6 @@ public class StatsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_stats);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        ThemeSelection.themeInitializer(findViewById(R.id.stats_activity_layout),this);
     }
 }
